06053142c815d2151cd99919763808c04356e968
labroid <orders@jacksonhome.net>
2021-02-01 20:07:22 -0500

    Deleted Pandas-based approach

--- txt_to_df.py
+++ txt_to_df.py
@@ -1,7 +1,5 @@
-import numpy as np
 import pandas as pd
 import arrow
-import io
 import re
 import csv
 from pydantic import BaseModel, validator
@@ -9,20 +7,6 @@ from datetime import datetime
 
 SCHWAB_FILE = r"C:\Users\Scott\Desktop\All-Accounts-Positions-2021-01-29-111408.CSV"
 FIDELITY_FILE = r"C:\Users\Scott\Desktop\Portfolio_Position_Jan-26-2021.csv"
-
-
-# COL_ORDER = [
-#     "date",
-#     "broker",
-#     "account",
-#     "symbol",
-#     "quantity",
-#     "price",
-#     "value",
-#     "basis",
-#     "kind",
-#     "qualified",
-# ]
 
 
 class Record(BaseModel):
@@ -73,64 +57,6 @@ def raw_schwab_to_df(schwab_file):
         return records
 
 
-# def raw_schwab_to_df2(schwab_file):
-#     with open(schwab_file) as f:
-#         schwab_raw = [r.replace("$", "") for r in f.readlines()]
-#     # Columns must be first row or read_csv fails or skips rows above idenified col row
-#     schwab_raw[0], schwab_raw[1] = schwab_raw[3], schwab_raw[0]
-#     # Pack it all up to look like a file for pd.read_csv
-#     buffer = io.StringIO()
-#     [buffer.write(f"{s}\n") for s in schwab_raw]
-#     buffer.seek(0)
-#
-#     schwab = pd.read_csv(
-#         buffer,
-#         usecols=["Symbol", "Quantity", "Price", "Market Value", "Cost Basis"],
-#         skip_blank_lines=True,
-#     )
-#     schwab.rename(
-#         columns={
-#             "Symbol": "symbol",
-#             "Quantity": "quantity",
-#             "Price": "price",
-#             "Market Value": "value",
-#             "Cost Basis": "basis",
-#         },
-#         inplace=True,
-#     )
-#     schwab = schwab.loc[schwab.symbol != "Symbol", :].copy()
-#     schwab = schwab.loc[schwab.symbol != "Account Total", :].copy()
-#     schwab_timestamp = arrow.get(schwab.iloc[0, 0], "hh:mm A[ ET, ]MM/DD/YYYY")
-#     schwab.drop(index=0, inplace=True)
-#     schwab[schwab == "--"] = np.nan
-#     schwab[schwab == "Incomplete"] = np.nan
-#     dollar_cols = ["quantity", "price", "value", "basis"]
-#     schwab[dollar_cols] = (
-#         schwab[dollar_cols]
-#             .applymap(lambda d: d.replace(",", ""), na_action="ignore")
-#             .astype(float)
-#             .copy()
-#     )
-#     schwab = schwab.fillna(0)
-#
-#     # At this point we have clean input data and begin augmenting it
-#     schwab["broker"] = "Schwab"
-#     schwab["account"] = schwab["symbol"].str.extract(r".*(XXXX-....).*")
-#     schwab["kind"] = None
-#     schwab["qualified"] = None
-#     schwab["date"] = schwab_timestamp
-#     schwab = schwab[COL_ORDER].copy()
-#     acct_filter = schwab.account.isna()
-#     schwab.account.ffill(inplace=True)
-#     schwab = schwab.loc[acct_filter, :].copy()
-#     schwab.replace("Cash & Cash Investments", "Cash", inplace=True)
-#     cash_filter = schwab.symbol == "Cash"
-#     schwab.loc[cash_filter, "price"] = 1
-#     schwab.loc[cash_filter, "quantity"] = schwab.loc[cash_filter, "value"]
-#     schwab.replace("No Number", "Golub", inplace=True)
-#     return schwab
-
-
 def raw_fidelity_to_df(fidelity_file):
     with open(fidelity_file) as f:
         records = [r for r in csv.reader(f)]
@@ -154,45 +80,3 @@ def raw_fidelity_to_df(fidelity_file):
             ).dict()
         )
     return answer
-
-
-# def raw_fidelity_to_df2(fidelity_file):
-#     fidelity = pd.read_csv(fidelity_file, skip_blank_lines=True)
-#     fidelity = fidelity.rename(
-#         columns={
-#             "Account Name/Number": "account",
-#             "Symbol": "symbol",
-#             "Quantity": "quantity",
-#             "Last Price": "price",
-#             "Current Value": "value",
-#             "Cost Basis Total": "basis",
-#         }
-#     )
-#     fidelity_timestamp = arrow.get(fidelity.iloc[-1, 0], "MM/DD/YYYY hh:mm A")
-#     # Pare down to needed columns
-#     fidelity = fidelity[
-#         ["account", "symbol", "quantity", "price", "value", "basis"]
-#     ].copy()
-#     # Eliminate unneeded rows
-#     fidelity = fidelity[fidelity.symbol.notnull()].copy()
-#     dollar_cols = ["price", "value", "basis"]
-#     fidelity[dollar_cols] = (
-#         fidelity[dollar_cols]
-#             .applymap(lambda d: d.replace("$", "").replace(",", ""), na_action="ignore")
-#             .astype(float)
-#             .copy()
-#     )
-#     fidelity = fidelity.fillna(0)
-#
-#     # At this point we have clean input data and begin augmenting it
-#     fidelity["broker"] = "fidelity"
-#     fidelity["kind"] = None
-#     fidelity["qualified"] = None
-#     fidelity["date"] = fidelity_timestamp
-#     fidelity = fidelity[COL_ORDER].copy()
-#     return fidelity
-
-
-fidelity = pd.DataFrame(raw_fidelity_to_df2(FIDELITY_FILE))
-schwab = pd.DataFrame(raw_schwab_to_df2(SCHWAB_FILE))
-print("Done")
